5dda14adae5c19b0866a78383d9bf505f3fce399
Paulo Marinho <paulo.marinho@personates.com>
2021-06-20 12:22:58 -0300

    2-coverage

---
+++ TestesIOSTests/ValidationServiceTests.swift
@@ -0,0 +1,70 @@
+//
+//  ValidationServiceTests.swift
+//  TestesIOSTests
+//
+//  Created by paulo marinho on 20/06/21.
+//  Copyright © 2021 Santander. All rights reserved.
+//
+
+@testable import TestesIOS
+import XCTest
+class ValidationServiceTests: XCTestCase {
+
+    var validation: ValidationService!
+    
+    override func setUp() {
+        super.setUp()
+        validation = ValidationService()
+    }
+
+    override func tearDown() {
+        validation = nil
+        super.tearDown()
+    }
+    
+    func test_is_valid_username() throws {
+        XCTAssertNoThrow(try validation.validateUsername("paulomarinho"))
+    }
+    
+    func test_username_is_nil() throws {
+        let expectedError = ValidationError.invalidValue
+        var error: ValidationError?
+        
+        XCTAssertThrowsError(try validation.validateUsername(nil)) { thrownError in
+            error = thrownError as? ValidationError
+        }
+        
+        XCTAssertEqual(expectedError, error)
+        
+        XCTAssertEqual(expectedError.errorDescription, error?.errorDescription)
+    }
+    
+    func test_username_too_short() throws {
+        let expectedError = ValidationError.usernameTooShort
+        var error: ValidationError?
+        
+        XCTAssertThrowsError(try validation.validateUsername("mar")) { thrownError in
+            error = thrownError as? ValidationError
+        }
+        
+        XCTAssertEqual(expectedError, error)
+        
+        XCTAssertEqual(expectedError.errorDescription, error?.errorDescription)
+    }
+    
+    func test_username_too_long() throws {
+        let expectedError = ValidationError.usernameTooLong
+        var error: ValidationError?
+        let username = "12345678901234567890"
+        
+        XCTAssertTrue(username.count == 20)
+        
+        XCTAssertThrowsError(try validation.validateUsername(username)) { thrownError in
+            error = thrownError as? ValidationError
+        }
+        
+        XCTAssertEqual(expectedError, error)
+        
+        XCTAssertEqual(expectedError.errorDescription, error?.errorDescription)
+    }
+}
